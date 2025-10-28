568e55303ec95581481282178539e4fab0bc4d2f
Ronen Aharon <RonenAa@elad.co.il>
2025-10-28 13:21:30 +0200

    final version

--- dimActionType.sql
+++ dimActionType.sql
@@ -1,24 +1,20 @@
-
-with Actiontypes as (
+with actiontype as(
 select distinct
-	TM.ActionType as  ActionType1,
-	G.AOPI_PEILOT,
-	act.ActionType as  ActionType2
+	TM.ActionType as Id,
+	case
+		when TM.ActionType = 1 then 'FOT'
+		when TM.ActionType = 11 then 'CIF'
+		else act.ActionType
+		end as 'ActionType'
 from TEODOT_MSHLOCH TM 
 	Left Join GORMIM G
 on TM.QOD_MQBL = G.QOD_GORM
 	Left join TBLT_PEOLOT_HZMNH_T_MSHLOCH act
 on TM.ActionType = act.MS_AOPTSIH
+where act.ActionType is not null
 )
 
-select 
-distinct
-ActionType1 as 'ActionId',
-CASE 
-	WHEN ActionType1 = 6 THEN AOPI_PEILOT
-	WHEN ActionType1 = 1 THEN 'FOT'
-	WHEN ActionType1 = 11 THEN 'CIF'
-	ELSE ActionType2
-	END		as 'ActionTypeDesc'
-from Actiontypes
-order by 1
+select *
+,
+RANK() over (order by id asc) as Sort
+from actiontype
