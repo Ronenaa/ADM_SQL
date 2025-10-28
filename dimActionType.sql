ba9444a993913c64f7a501b145464cc99919c973
Ronen Aharon <RonenAa@elad.co.il>
2025-10-28 17:04:49 +0200

    new version

--- dimActionType.sql
+++ dimActionType.sql
@@ -1,20 +1,9 @@
-with actiontype as(
-select distinct
-	TM.ActionType as Id,
-	case
-		when TM.ActionType = 1 then 'FOT'
-		when TM.ActionType = 11 then 'CIF'
-		else act.ActionType
-		end as 'ActionType'
-from TEODOT_MSHLOCH TM 
-	Left Join GORMIM G
-on TM.QOD_MQBL = G.QOD_GORM
-	Left join TBLT_PEOLOT_HZMNH_T_MSHLOCH act
-on TM.ActionType = act.MS_AOPTSIH
-where act.ActionType is not null
-)
-
-select *
-,
-RANK() over (order by id asc) as Sort
-from actiontype
+select 
+MS_AOPTSIH as Id,
+CASE
+		WHEN MS_AOPTSIH = 1 THEN 'FOT'
+		WHEN MS_AOPTSIH = 11 THEN 'CIF'
+		ELSE ActionType
+		END AS ActionType,
+RANK() OVER (ORDER BY MS_AOPTSIH) AS Sort
+from TBLT_PEOLOT_HZMNH_T_MSHLOCH
